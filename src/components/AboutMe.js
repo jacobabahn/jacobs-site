b234fba5f69409aad7eda33df207ed45e16c4837
Jacob Bahn <jacobabahn@gmail.com>
2022-05-23 14:34:34 -0400

    Modified styling for the About page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,15 +15,17 @@ const AboutMe = (props) => {
         <div className="page" id="about">
             <Fade left>
                 <h1 className="heading">Who am I?</h1>
-                {/* <div className="content"> */}
                 <Card className="content" elevation={11}>
-                    <img className="me" src={me} />
-                    <p className="ptext">
-                        I am a Masters student studying Computer Science. 
-                        My hobbies include working out, basketball, football, cars, and of course, technology.
-                        Some development languages I've worked with are Python, JavaScript(React), TypeScript, and C++. <br/> <br/>
-                        Feel free to contact me at jacobabahn@southern.edu!
-                    </p>
+                    <div className="imgDiv">
+                        <img className="me" src={me} />
+                    </div>
+                    <div className="textContainer">
+                        <p className="ptext">
+                            I am a Masters student studying Computer Science. 
+                            My hobbies include working out, basketball, football, cars, and of course, technology.
+                            Some development languages I've worked with are Python, JavaScript(React), TypeScript, and C++. <br/> <br/>
+                        </p>
+                    </div>
                 </Card>
             </Fade>
             <Link to="proj" smooth={true} duration={1000}>
